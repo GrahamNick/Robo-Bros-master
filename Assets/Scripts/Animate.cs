1659b2551b58f1b1ab469f0ca1ce4441d3b2265a
GrahamNick <37307748+GrahamNick@users.noreply.github.com>
2019-03-08 22:07:15 -0500

    Initial commit

---
+++ Assets/Scripts/Animate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+	private Animator anim;
+	public GameObject player;
+	private float dis;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		dis = Mathf.Abs (transform.position.x - player.transform.position.x);
+		anim.SetFloat ("distance", dis);
+		Debug.Log (dis);
+
+	}
+}
---
+++ Assets/Scripts/returnAfterFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class returnAfterFall : MonoBehaviour {
+	private Vector3 startpos;
+	// Use this for initialization
+	void Start () {
+		startpos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <=  -2) {
+			transform.position = startpos;
+		}
+	}
+}
